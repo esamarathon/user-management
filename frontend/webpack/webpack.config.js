72538fe8363fc17c9a5cd904a64991d763fe0372
CBenni <levelcarnage@gmail.com>
2019-03-25 16:19:07 +0100

    Fixed webpack config: scss hot-reload, source maps

--- frontend/webpack/webpack.config.js
+++ frontend/webpack/webpack.config.js
@@ -2,40 +2,44 @@ const path = require('path');
 const loaders = require('./loaders');
 const plugins = require('./plugins');
 
-module.exports = {
-  entry: ['./src/main.js'],
-  module: {
-    rules: [
-      loaders.VueLoader,
-      loaders.CSSLoader,
-      loaders.SCSSLoader,
-      loaders.JSLoader,
-      loaders.RawLoader,
-      loaders.ESLintLoader,
-      loaders.FileLoader,
-    ],
-  },
-  plugins: [
-    plugins.MiniCssExtractPlugin,
-    plugins.VueLoaderPlugin,
-    plugins.HTMLWebpackPlugin,
-  ],
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: '[hash].bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.vue', '.json', '.scss'],
-    alias: {
-      vue$: 'vue/dist/vue.esm.js',
-      '@': './src',
-      $: path.join(__dirname, '../node_modules'),
+module.exports = (env, argv) => {
+  console.log(`Using webpack ${argv.mode} environment.`);
+  return {
+    entry: ['./src/main.js'],
+    module: {
+      rules: [
+        loaders.VueLoader,
+        loaders.CSSLoader,
+        loaders.SCSSLoader(env, argv),
+        loaders.JSLoader,
+        loaders.RawLoader,
+        loaders.ESLintLoader,
+        loaders.FileLoader,
+      ],
     },
-  },
-  devServer: {
-    allowedHosts: [
-      '127.0.0.1',
+    plugins: [
+      plugins.MiniCssExtractPlugin,
+      plugins.VueLoaderPlugin,
+      plugins.HTMLWebpackPlugin,
     ],
-    historyApiFallback: true,
-  },
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: '[hash].bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.vue', '.json', '.scss'],
+      alias: {
+        vue$: 'vue/dist/vue.esm.js',
+        '@': './src',
+        $: path.join(__dirname, '../node_modules'),
+      },
+    },
+    devServer: {
+      allowedHosts: [
+        '127.0.0.1',
+      ],
+      historyApiFallback: true,
+    },
+    devtool: 'source-map',
+  };
 };
